252daaf85cf24153b2779c03554c0687fadb41ed
cpasjuste <cpasjuste@gmail.com>
2014-11-25 10:26:55 +0100

    fix compilation

--- common.mk
+++ common.mk
@@ -1,19 +1,19 @@
 #Common headers
-common_includes := hardware/qcom/display/libgralloc
-common_includes += hardware/qcom/display/liboverlay
-common_includes += hardware/qcom/display/libcopybit
-common_includes += hardware/qcom/display/libqdutils
-common_includes += hardware/qcom/display/libhwcomposer
-common_includes += hardware/qcom/display/libexternal
-common_includes += hardware/qcom/display/libqservice
-common_includes += hardware/qcom/display/libvirtual
+common_includes := hardware/qcom/display-caf-hdx/libgralloc
+common_includes += hardware/qcom/display-caf-hdx/liboverlay
+common_includes += hardware/qcom/display-caf-hdx/libcopybit
+common_includes += hardware/qcom/display-caf-hdx/libqdutils
+common_includes += hardware/qcom/display-caf-hdx/libhwcomposer
+common_includes += hardware/qcom/display-caf-hdx/libexternal
+common_includes += hardware/qcom/display-caf-hdx/libqservice
+common_includes += hardware/qcom/display-caf-hdx/libvirtual
 
 ifeq ($(TARGET_USES_POST_PROCESSING),true)
     common_flags     += -DUSES_POST_PROCESSING
     common_includes  += $(TARGET_OUT_HEADERS)/pp/inc
 endif
 
-common_header_export_path := qcom/display
+common_header_export_path := qcom/display-caf-hdx
 
 #Common libraries external to display HAL
 common_libs := liblog libutils libcutils libhardware
